82eeb1ac18d7e3a365b75e52fb146a9e8ab58bed
Git-Ananda <anandaroop.maitra.work@gmail.com>
2026-07-14 12:54:09 +0530

    Explicit wait increased to 20 sec

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -1,9 +1,14 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DashboardPage extends BasePage {
+
+    private static final Logger logger = LogManager.getLogger(DashboardPage.class);
+
 
     private final By dashboardHeading = By.xpath("//h6[text()='Dashboard']");
 
@@ -14,6 +19,8 @@ public class DashboardPage extends BasePage {
     }
 
     public boolean isDashboardDisplayed() {
+
+        logger.info("Checking dashboard visibility.");
 
         return waitUtility.waitForVisibility(dashboardHeading).isDisplayed();
 
