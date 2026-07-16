69189101a91a04ca1cc893119f020d793b5a256e
Git-Ananda <anandaroop.maitra.work@gmail.com>
2026-07-16 12:31:13 +0530

    Add headless browser support for Jenkins

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -2,8 +2,13 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.openqa.selenium.remote.Browser.CHROME;
+import static org.openqa.selenium.remote.Browser.EDGE;
 
 public final class DriverFactory {
 
@@ -20,9 +25,38 @@ public final class DriverFactory {
 
         return switch (browser.toLowerCase()) {
 
-            case "chrome" -> new ChromeDriver();
+            case CHROME:
 
-            case "edge" -> new EdgeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                chromeOptions.addArguments("--headless=new");
+
+                chromeOptions.addArguments("--disable-gpu");
+
+                chromeOptions.addArguments("--window-size=1920,1080");
+
+                chromeOptions.addArguments("--remote-allow-origins=*");
+
+                chromeOptions.addArguments("--no-sandbox");
+
+                yield new ChromeDriver(chromeOptions);
+
+            case EDGE:
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+
+                edgeOptions.addArguments("--headless=new");
+
+                edgeOptions.addArguments("--disable-gpu");
+
+                edgeOptions.addArguments("--window-size=1920,1080");
+
+                edgeOptions.addArguments("--remote-allow-origins=*");
+
+                edgeOptions.addArguments("--no-sandbox");
+
+                yield new EdgeDriver(edgeOptions);
+
 
             case "firefox" -> new FirefoxDriver();
 
